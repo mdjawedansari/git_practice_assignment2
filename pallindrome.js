f517e09d41a00f9360fdeffdc973f39dfd5b6b03
Maneesha Kumari <manithakur1224@gmail.com>
2022-11-30 19:57:46 +0530

    code_fixed

--- pallindrome.js
+++ pallindrome.js
@@ -3,8 +3,10 @@ function pallindrome(str){
   for(let i=str.length-1;i>=0;i--){
     bag+=str[i];
   }
-  if(count==str[i]){
+  if(bag==str){
     console.log(str,"is pallindrome");
+  }else{
+    console.log(str,"not a pallindrome");
   }
 }
 pallindrome("masai");
