061b21eab0e1e4f0d10a992f7ca2c6c7ceae99d3
jawed0216 <jawed0216@gmail.com>
2022-11-30 19:41:53 +0530

    I have written the code

---
+++ pallindrome.js
@@ -0,0 +1,10 @@
+function pallindrome(str){
+  let bag="";
+  for(let i=str.length-1;i>=0;i--){
+    bag+=str[i];
+  }
+  if(count==str[i]){
+    console.log(str,"is pallindrome");
+  }
+}
+pallindrome("masai");
---
+++ prime.js
@@ -0,0 +1,12 @@
+function prime(num){
+  let count=0;
+  for(let i=1;i<=num;i++){
+    if(num%i==0){
+      
+    }
+  }
+  if(bag==2){
+    console.log(num,"is Prime");
+  }
+}
+prime(12);
